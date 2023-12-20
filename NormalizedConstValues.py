846e6fcf060131a66b6157ca459faa27dcd46b91
John-Crni <130535063+John-Crni@users.noreply.github.com>
2023-12-20 12:28:46 +0900

    Add files via upload

---
+++ NormalizedConstValues.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class SelialValues(enum):
+    none="none"
+    enable="enable"
+    conected="conected"
+    disconected="disconected"
+    notconect="notconect"
+    unkowm="unkowm"
+    
+class StarNames(enum):
+    sun="sun"
+    mercury="mercury"
+    venus="venus"
+    mars="mars"
+    
+    
+    
